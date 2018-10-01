5448d42b482a9e9a9358a7b10c436b1da9563a74
Soubhik_sk83 <soubhik.khan@gmail.com>
2018-10-01 11:55:25 +0530

    msm8953-common: Uprev audio, audio.effect, health, soundtrigger HALs

--- msm8953.mk
+++ msm8953.mk
@@ -80,11 +80,11 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.soundtrigger@2.0-service \
     audio.a2dp.default \
     audio.primary.msm8953 \
@@ -193,6 +193,7 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service \
     chargeonlymode
 
 # HIDL
